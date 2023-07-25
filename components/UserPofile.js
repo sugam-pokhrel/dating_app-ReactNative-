a3de8707f9e98c8fda147919b5f6bedf3c9d6f3a
Sugam Pokhrel <psugam75@gmail.com>
2023-07-25 16:59:22 +0545

    added

--- components/UserPofile.js
+++ components/UserPofile.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HomeScreen(){
 return(
     <View style={styles.container}>
-      <Text>This is the home Screen</Text>
+      <Text>This is the user Profile</Text>
       
     </View>
 )
